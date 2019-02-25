643622f81431ec5b05dc3918b37efb9c6fa56f9e
Anthony <yihonganthony@gmail.com>
2019-02-24 22:56:38 -0600

    added ikea and apple scripts, tried spotify

--- appletimeline.r
+++ appletimeline.r
@@ -5,47 +5,34 @@ library(glue)
 library(rnaturalearth)
 library(gifski)
 
-url <- "https://en.wikipedia.org/wiki/Spotify"
+url <- "https://en.wikipedia.org/wiki/Apple_Store"
 # confirms url
 url
 
 # reads in the HTML table from wikipedia
 table <- url %>%
-  read_html() %>%
-  html_nodes(xpath = '/html/body/div[3]/div[3]/div[4]/div/table[6]') %>%
-  html_table()  %>% .[[1]]
+read_html() %>%
+html_nodes(xpath = '/html/body/div[3]/div[3]/div[4]/div/table[2]') %>%
+html_table()  %>% .[[1]]
 
 # extracts the country and year columns from the data
-table <- table[1:2]
-
-table
+table <- table[2:3]
 
 # learning: checks type of data the table is, changes the column name to something more friendly
 class(table[2])
-colnames(table)[1] <- "Year"
-colnames(table)[2] <- "Country"
+colnames(table)[1] <- "Country"
+colnames(table)[2] <- "Year"
 
-# separates multiple countries in one row into their own row with date
-table <- separate_rows(table, Country, sep="\n ")
-
+# removes any additional details about countries in parenthesis
+table[1]$Country <- str_replace(table[1]$Country, " \\(.*", "")
+table[1]$Country <- str_replace(table[1]$Country, "\\(.*", "")
+# remove citations [13]
+table[2]$Year <- str_replace(table[2]$Year, "\\[.*", "")
 # extracts only the opening year from the full date
-# table[1]$Year <- as.numeric(str_sub(table[1]$Year, -4, -1))
-
-### Section to take both months and year of the expansion
-### Can't get function to iterate by month
-# replaces first few ints to get month and year only
-# table[1]$Year <- str_replace(table[1]$Year, "\\d+\\s+", "")
-# table[1]$Year <- paste0("1 ", table[1]$Year)
-# table[1]$Year <- as.Date(table[1]$Year, "%d %B %Y")
-
-table
-
-# TO DO: 
-# Obtain the month and year https://www.statmethods.net/input/dates.html
-# plot using month and year
+table[2]$Year <- as.numeric(str_sub(table[2]$Year, -4, -1))
 
 table[is.na(table)] = "Soon"
-# removes any invalid cells
+# removes any cells that aren't open yet
 table = table[ !grepl("Soon", table$Year) , ]
 
 table
@@ -58,7 +45,8 @@ mismatches
 
 country_match <- tribble(
   ~Country, ~sf_country,
-  "United States", "United States of America")
+  "United States", "United States of America",
+  "China", "People's Republic of China")
 country_match
 
 
@@ -71,20 +59,20 @@ joined_data <-
 
 ggplot() + geom_sf(data = countries_sf) 
 
-# plots spotify on the map
+# plots the opening of each apple store on the map, code from https://r-mageddon.netlify.com/post/the-burger-king-pandemic/
 plot_fun <- function(open_year){
   p <- ggplot() + 
     ## Add the background countries, but filter away Antarctica because it is unecessary.
     geom_sf(data = filter(countries_sf, name_en != "Antarctica")) + 
-    ## Add the countries data, filtering so countries only appear after spotify has joined
-    geom_sf(data = filter(joined_data, Year <= open_year), fill = rgb(30, 215, 96, maxColorValue = 255)) + 
+    ## Add the countries data, filtering so countries only appear after it opened
+    geom_sf(data = filter(joined_data, Year <= open_year), fill = rgb(0, 0, 0, maxColorValue = 255)) + 
     ## Change the theme so we don't get a background plot or axes. The coord_sf part is a workaround to a current bug that makes gridlines appear.
     theme_void() + coord_sf(datum=NA) + guides(fill=FALSE) + 
     ## Stitch the year to the title with glue
     labs(title = glue("    Year: {open_year}"),
-         subtitle = glue("      Countries with Spotify: {nrow(filter(joined_data, Year <= open_year))}"))
+         subtitle = glue("      Countries with Apple Stores: {nrow(filter(joined_data, Year <= open_year))}"))
   print(p)
 }
 
 # saves visualization as a gif
-save_gif(walk(min(joined_data$Year):max(joined_data$Year), plot_fun), delay = 0.5, gif_file = "spotify.gif")
+save_gif(walk(min(joined_data$Year):max(joined_data$Year), plot_fun), delay = 0.6, gif_file = "applestore.gif")
--- ikeatimeline.r
+++ ikeatimeline.r
@@ -5,47 +5,34 @@ library(glue)
 library(rnaturalearth)
 library(gifski)
 
-url <- "https://en.wikipedia.org/wiki/Spotify"
+url <- "https://en.wikipedia.org/wiki/List_of_countries_with_IKEA_stores"
 # confirms url
 url
 
 # reads in the HTML table from wikipedia
 table <- url %>%
-  read_html() %>%
-  html_nodes(xpath = '/html/body/div[3]/div[3]/div[4]/div/table[6]') %>%
-  html_table()  %>% .[[1]]
+read_html() %>%
+html_nodes(xpath = '/html/body/div[3]/div[3]/div[4]/div/table[1]') %>%
+html_table()  %>% .[[1]]
 
 # extracts the country and year columns from the data
 table <- table[1:2]
-
 table
+
 
 # learning: checks type of data the table is, changes the column name to something more friendly
 class(table[2])
-colnames(table)[1] <- "Year"
-colnames(table)[2] <- "Country"
+colnames(table)[1] <- "Country"
+colnames(table)[2] <- "Year"
 
-# separates multiple countries in one row into their own row with date
-table <- separate_rows(table, Country, sep="\n ")
-
+# remove citations [13]
+table[2]$Year <- str_replace(table[2]$Year, " \\(.*", "")
+table[2]$Year <- str_replace(table[2]$Year, "\\(.*", "")
 # extracts only the opening year from the full date
-# table[1]$Year <- as.numeric(str_sub(table[1]$Year, -4, -1))
-
-### Section to take both months and year of the expansion
-### Can't get function to iterate by month
-# replaces first few ints to get month and year only
-# table[1]$Year <- str_replace(table[1]$Year, "\\d+\\s+", "")
-# table[1]$Year <- paste0("1 ", table[1]$Year)
-# table[1]$Year <- as.Date(table[1]$Year, "%d %B %Y")
-
-table
-
-# TO DO: 
-# Obtain the month and year https://www.statmethods.net/input/dates.html
-# plot using month and year
+table[2]$Year <- as.numeric(str_sub(table[2]$Year, -4, -1))
 
 table[is.na(table)] = "Soon"
-# removes any invalid cells
+# removes any cells listing McDonald's that aren't open yet
 table = table[ !grepl("Soon", table$Year) , ]
 
 table
@@ -58,33 +45,36 @@ mismatches
 
 country_match <- tribble(
   ~Country, ~sf_country,
-  "United States", "United States of America")
+  "United States", "United States of America",
+  "China", "People's Republic of China")
 country_match
 
 
 # joins the data frames to fix name mismatches
-joined_data <- 
+ikea_joined_data <- 
   table %>%
   left_join(country_match) %>%
   mutate(country = ifelse(is.na(sf_country), Country, sf_country)) %>%
   left_join(countries_sf, by  = c("country" = "name_en"))
 
+ikea_joined_data
+
 ggplot() + geom_sf(data = countries_sf) 
 
-# plots spotify on the map
+# plots the opening of each mcdonalds on the map, code from https://r-mageddon.netlify.com/post/the-burger-king-pandemic/
 plot_fun <- function(open_year){
   p <- ggplot() + 
     ## Add the background countries, but filter away Antarctica because it is unecessary.
     geom_sf(data = filter(countries_sf, name_en != "Antarctica")) + 
-    ## Add the countries data, filtering so countries only appear after spotify has joined
-    geom_sf(data = filter(joined_data, Year <= open_year), fill = rgb(30, 215, 96, maxColorValue = 255)) + 
+    ## Add the countries data, filtering so countries only appear after the first mcdonalds has opened
+    geom_sf(data = filter(ikea_joined_data, Year <= open_year), fill = rgb(0, 81, 186, maxColorValue = 255)) + 
     ## Change the theme so we don't get a background plot or axes. The coord_sf part is a workaround to a current bug that makes gridlines appear.
     theme_void() + coord_sf(datum=NA) + guides(fill=FALSE) + 
     ## Stitch the year to the title with glue
     labs(title = glue("    Year: {open_year}"),
-         subtitle = glue("      Countries with Spotify: {nrow(filter(joined_data, Year <= open_year))}"))
+         subtitle = glue("      Countries with IKEA's: {nrow(filter(ikea_joined_data, Year <= open_year))}"))
   print(p)
 }
 
 # saves visualization as a gif
-save_gif(walk(min(joined_data$Year):max(joined_data$Year), plot_fun), delay = 0.5, gif_file = "spotify.gif")
+save_gif(walk(min(ikea_joined_data$Year):max(ikea_joined_data$Year), plot_fun), delay = 0.6, gif_file = "ikea.gif")
